77752023045cbe443ee6fae1f44773de8d6be566
ammarafridi-max <ammar.afridi95@gmail.com>
2024-07-03 17:12:15 +0400

    Removed activities routes

--- api/index.js
+++ api/index.js
@@ -4,8 +4,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,13 +29,11 @@ mongoose
   });
 
 // Routes
-// const activitiesRoutes = require("./routes/activities-routes");
 const userRoutes = require("../routes/user-routes");
 const blogRoutes = require("../routes/blog-routes");
 const tourRoutes = require("../routes/tours-routes");
 const destinationRoutes = require("../routes/destination-routes");
 
-// app.use("/activities", activitiesRoutes);
 app.use("/users", userRoutes);
 app.use("/blogs", blogRoutes);
 app.use("/tours", tourRoutes);
